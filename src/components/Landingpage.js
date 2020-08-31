638f582f59081e2040f6d67e75eb75536de23f4d
CodingwithJess <thejessiebess@gmail.com>
2020-08-31 17:30:54 -0600

    registered service worker, updated syling on app.css and index.css

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,11 +1,44 @@
-import React, {Component} from "react";
-import {Grid, Cell} from "react-mdl";
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
-  render () {
-    return(
+  render() {
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://avatars1.githubusercontent.com/u/64941746?s=460&u=6f65f27759f3f66a20ba237b6a8f457428015ff9&v=4"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+              <hr />
+              <p>
+                HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express |
+                MongoDB
+              </p>
+              <div className="social-links">
+                {/* LinkedIn */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
 
-    )
+                {/* Github */}
+                <a
+                  href="http://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
   }
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,19 +2,19 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import LandingPage from "./Landingpage";
-import AboutMe from "./Aboutme";
-import Contact from "./Contact";
-import Projects from "./Projects";
-import Resume from "./Resume";
+// import AboutMe from "./Aboutme";
+// import Contact from "./Contact";
+// import Projects from "./Projects";
+// import Resume from "./Resume";
 
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
-    <Route path="/aboutme" component={AboutMe} />
+    {/* <Route path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
-    <Route path="/resume" component={Resume} />
+    <Route path="/resume" component={Resume} /> */}
   </Switch>
 )
 
