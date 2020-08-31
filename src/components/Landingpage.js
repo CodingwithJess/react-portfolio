ae7787752925ec2c48cfd6f203ff2a5de9a56e27
CodingwithJess <thejessiebess@gmail.com>
2020-08-31 11:56:11 -0600

    updated Main.js and added new component for Landingpage.js, started basic structure

---
+++ src/components/Landingpage.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {Grid, Cell} from "react-mdl";
+
+class Landing extends Component {
+  render () {
+    return(
+
+    )
+  }
+}
+
+export default Landing;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import LandingPage from './landingpage';
-import AboutMe from './aboutme';
-import Contact from './contact';
-import Projects from './projects';
-import Resume from './resume';
+import LandingPage from "./Landingpage";
+import AboutMe from "./Aboutme";
+import Contact from "./Contact";
+import Projects from "./Projects";
+import Resume from "./Resume";
 
 
 const Main = () => (
