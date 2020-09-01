c27e47c2023ae35c1df5032e7c4d22f57e81700b
CodingwithJess <thejessiebess@gmail.com>
2020-08-31 22:24:52 -0600

    Updates

---
+++ precache-manifest.7c80562fc7a0bcda8a24a850f72e7a24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de401ffdd71b8a47e23226ab6cee514",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d10f0fe15a0add6c8b45",
+    "url": "/react-portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "db36cf61f6bb181fa3ef",
+    "url": "/react-portfolio/static/css/main.0cd8f19e.chunk.css"
+  },
+  {
+    "revision": "d10f0fe15a0add6c8b45",
+    "url": "/react-portfolio/static/js/2.21978199.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-portfolio/static/js/2.21978199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db36cf61f6bb181fa3ef",
+    "url": "/react-portfolio/static/js/main.42318da7.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "1d34e41dca39d86299e3d60690eca542",
+    "url": "/react-portfolio/static/media/Sparkle.1d34e41d.jpg"
+  },
+  {
+    "revision": "e44a3c4d8c7469b59c29d2f03ef988f8",
+    "url": "/react-portfolio/static/media/logo.e44a3c4d.png"
+  },
+  {
+    "revision": "bd97554f60f92b6c4f154b26045685e8",
+    "url": "/react-portfolio/static/media/profile.bd97554f.jpeg"
+  }
+]);
