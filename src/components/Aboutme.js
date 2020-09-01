0710082a661fb82e66c397a3f3e9e810c19d230a
CodingwithJess <thejessiebess@gmail.com>
2020-08-31 23:07:54 -0600

    updated about me page

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,45 +1,49 @@
 import React, { Component } from "react";
 import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
-import img from "../img/profile.jpeg"
+import img from "../img/profile.jpeg";
 
 class About extends Component {
   render() {
-    return(
+    return (
       <div className="about-body">
         <Grid className="about-grid">
-        <Cell col={12}>
+          <Cell col={12}>
             <h2>About Me</h2>
-            <hr />
-        </Cell>
+          </Cell>
           <Cell col={6}>
             <img
               src={img}
               alt="selfie"
-              style={{ height: "350px", borderRadius:"50%" }}
+              style={{ height: "350px"}}
             />
           </Cell>
           <Cell col={6}>
-            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries
+            <p style={{ width: "85%", margin: "auto", paddingTop: "1em", fontSize:"18px"}}>
+            My name is Jessica and I am currently in the fulltime coding program through the University of Denver. We are currently learning React.js. I am looking forward to learning more and expanding my skills and knowledge on a daily basis.
+            
+            </p>
+            <p style={{ width: "85%", margin: "auto", paddingTop: "1em",fontSize:"18px" }}>
+            
+            After finishing class, I hope to find a full time Junior Web Developer job. I am open to relocating almost anywhere in the US! I appreciate companies that are open to training on the job, have collaborative teams, and believe in open communication and having fun!
             </p>
           </Cell>
-            <br/>
-          <br/>
-            <hr />
+          <hr />
           <Cell col={12}>
-            <h2>Resume</h2>
             <hr />
-            <div className="contact-list">
+            <h2>Resume</h2>
+            <div className="about-list">
               <List>
                 <ListItem>
                   <ListItemContent
-                    style={{ fontSize: "20px", fontFamily: "Didact Gothic", color:"#eebe68"}}>
-                    <i class="far fa-file"/>
-                    <br/>
+                    style={{
+                      fontSize: "20px",
+                      fontFamily: "Didact Gothic",
+                      color: "#eebe68",
+                      textAlign: "center"
+                    }}
+                  >
+                    <i class="far fa-file" />
+                    <br />
                     PDF
                   </ListItemContent>
                 </ListItem>
@@ -48,7 +52,7 @@ class About extends Component {
           </Cell>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
