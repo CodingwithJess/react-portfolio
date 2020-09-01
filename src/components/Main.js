f9d60efdabd77d0c3721ce9591205d5365f35d1c
CodingwithJess <thejessiebess@gmail.com>
2020-08-31 19:28:01 -0600

    added contact page component and added dummy data

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 import LandingPage from "./Landingpage";
 import AboutMe from "./Aboutme";
-// import Contact from "./Contact";
+import Contact from "./Contact";
 import Projects from "./Projects";
 // import Resume from "./Resume";
 
@@ -12,7 +12,7 @@ const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
     <Route path="/aboutme" component={AboutMe} />
-    {/* <Route path="/contact" component={Contact} /> */}
+    <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
     {/* <Route path="/resume" component={Resume} /> */}
   </Switch>
