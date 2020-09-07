b188cb6dc28c6659f3377667d768b802ab8a3512
CodingwithJess <thejessiebess@gmail.com>
2020-09-06 18:21:50 -0600

    layout updates

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -21,10 +21,28 @@ class About extends Component {
             />
           </Cell>
           <Cell col={6}>
-              <p>
-                HTML/CSS | Bootstrap | JavaScript | MongoDB | Express | React |
-                Node›
-              </p>
+            <hr/>
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+            Coding makes me feel like a witch. 
+            <br/> <br/>
+            I remember the first time I built a good looking and responsive website from scratch.   
+            <br/><br/>*BOOM* <br/><br/>
+            Magic. Right before my eyes.
+            <br/>Every time I sit down to code I am in
+            awe of the endless possibilities, limited only by the speed at
+            which I'm able to learn and apply a new language or framework.  
+            <br/><br/> I am an aspiring full-stack web developer who has a deep passion for
+            brewing up web designs, conquering bugs, integrating fully
+            responsive mobile friendly sites, building servers, incorporating
+            API's, mastering git collaboration, and continuing to learn in a
+            fast changing industry.  
+            <br/> <br/>Full-stack web development also calls to
+            many of my best qualities. The artistic and endlessly creative
+            side of Front End, and the analytical and robust side of Back End.
+            Coding has me spellbound and I cannot wait to see where the
+            journey takes me.
+          </p>
+            <hr/>
           </Cell>
         </Grid>
       </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,29 +17,8 @@ class Contact extends Component {
           <img
             src="https://avatars1.githubusercontent.com/u/64941746?s=460&u=6f65f27759f3f66a20ba237b6a8f457428015ff9&v=4"
             alt="selfie"
-            style={{ height: "250px", borderRadius: "50%" }}
+            style={{ height: "250px", borderRadius: "50%", alignItems: "center" }}
           />
-          <hr />
-          <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
-            Coding makes me feel like a witch. 
-            <br/> <br/>
-            I remember the first time I built a good looking and responsive website from scratch.   
-            <br/><br/>*BOOM* <br/><br/>
-            Magic. Right before my eyes.
-            <br/>Every time I sit down to code I am in
-            awe of the endless possibilities, limited only by the speed at
-            which I'm able to learn and apply a new language or framework.  
-            <br/><br/> I am an aspiring full-stack web developer who has a deep passion for
-            brewing up web designs, conquering bugs, integrating fully
-            responsive mobile friendly sites, building servers, incorporating
-            API's, mastering git collaboration, and continuing to learn in a
-            fast changing industry.  
-            <br/> <br/>Full-stack web development also calls to
-            many of my best qualities. The artistic and endlessly creative
-            side of Front End, and the analytical and robust side of Back End.
-            Coding has me spellbound and I cannot wait to see where the
-            journey takes me.
-          </p>
         </Cell>
         <Cell col={6}>
           <div className="contact-list">
